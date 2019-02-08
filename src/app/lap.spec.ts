97a061652c6deaf4f939192e68aae35d781b3e70
John Franti <jfranti@gmail.com>
2019-02-08 10:42:45 -0800

    lap time converts to seconds

--- src/app/lap.spec.ts
+++ src/app/lap.spec.ts
@@ -2,6 +2,24 @@ import { Lap } from './lap';
 
 describe('Lap', () => {
   it('should create an instance', () => {
-    expect(new Lap()).toBeTruthy();
+    expect(new Lap("1:29.100")).toBeTruthy();
   });
+
+  it('should accept a lap time', () => {
+    expect (new Lap("1:29.100")).toBeTruthy();
+  })
+
+  it('should convert a string of 1 second to seconds', () => {
+    expect (new Lap('0:01.00').timeInSeconds()).toEqual(1);
+  })
+
+  it('should convert a string of 1 minute 1 second to seconds', () => {
+    expect(new Lap('1:01.00').timeInSeconds()).toEqual(61);
+  })
+
+  it('should convert a string of 1 minute 1 second and 23 hundreths to seconds', () => {
+    expect(new Lap('1:01.023').timeInSeconds()).toEqual(61.023);
+  })
+
+
 });
--- src/app/lap.ts
+++ src/app/lap.ts
@@ -1,2 +1,22 @@
 export class Lap {
+    constructor(public lapTimeString: string){}
+
+    timeInSeconds(){
+        let minutes;
+        let seconds;
+        minutes = parseFloat(this.lapTimeString.match(/.+?(?=:)/).toString()); 
+        seconds = parseFloat(this.lapTimeString.match(/(?<=:).*/).toString()); 
+
+        console.log("seconds raw: " + this.lapTimeString.match(/(?<=:).*/));
+        console.log("minutes: " + minutes);
+        console.log("seconds: " + seconds);
+        
+        
+        let totalTime = parseFloat((minutes*60)+seconds);
+        console.log(totalTime);
+        
+        
+        return totalTime;
+
+    }
 }
