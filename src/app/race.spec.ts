cf779f046589aa3417a079375de6ac53f889a7f3
John Franti <jfranti@gmail.com>
2019-02-08 14:16:39 -0800

    add some race logic

--- src/app/race.spec.ts
+++ src/app/race.spec.ts
@@ -1,7 +1,14 @@
 import { Race } from './race';
 
 describe('Race', () => {
+  let testRace: Race;
+  beforeEach(() => {
+    testRace = new Race("Brno", 90);
+  })
   it('should create an instance', () => {
-    expect(new Race()).toBeTruthy();
+    expect(testRace).toBeTruthy();
   });
+  it('should return expected laps for Brno', () => {
+    expect(testRace.getEstimatedLaps(120)).toEqual(45);
+  })
 });
--- src/app/race.ts
+++ src/app/race.ts
@@ -1,2 +1,12 @@
 export class Race {
+    public laps: number;
+    public time: number; //race length in seconds
+
+    constructor(public track: string, public timeInMinutes: number) {
+        this.time = timeInMinutes*60;
+    }
+
+    getEstimatedLaps(myAverageLapTimeInSeconds: number) {
+        return Math.ceil(this.time/myAverageLapTimeInSeconds);
+    }
 }
